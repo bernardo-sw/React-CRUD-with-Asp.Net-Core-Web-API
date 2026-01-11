7206235b5eb427d6a23851fa065ac1bab1f9e6a9
Bernardo Albuquerque <bernardo-sw@outlook.com>
2026-01-11 01:59:16 -0300

    A touch of style.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
+import net_core_logo from './NET_Core_Logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="container-logos">
+          <img src={logo} className="App-logo" alt="React Logo" />
+          <img src={net_core_logo} className="Net-Core-logo" alt=".NET Core Logo" />
+        </div>
         <h1 className='Uppercase'>Bernardo Albuquerque</h1>
         <p>
           Full Stack Developer | Software Engineer | Tech Enthusiast
