f1e0c36e0a9e5c33552bcfea8e3cf0807e5e3998
Bernardo Albuquerque <bernardo-sw@outlook.com>
2026-01-28 18:25:14 -0300

    Added student inclusion and update actions to the React app.

--- react-app/src/components/ManageStudent/index.js
+++ react-app/src/components/ManageStudent/index.js
@@ -1,10 +1,74 @@
-import React from 'react';
-import {Link, useParams} from 'react-router-dom';
-import {FiUserPlus, FiCornerDownLeft} from 'react-icons/fi';
+import React, { useMemo, useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { FiUserPlus, FiCornerDownLeft } from 'react-icons/fi';
+import api from '../../services/api';
 import './styles.css';
 
 export default function ManageStudent() {
+    
+    const [studentId, setStudentId] = useState(null);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [age, setAge] = useState(0);
+
     const {id} = useParams();
+    const navigate = useNavigate();
+
+    const token = localStorage.getItem('token');
+    const authorization = useMemo(() => ({
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    }), [token]);
+
+    useEffect(() => {
+        if (id === '0')
+            return;
+
+        async function loadStudentData(studentId) {
+            try {
+                const response = await api.get(`api/students/${studentId}`, authorization);
+                setStudentId(response.data.id);
+                setName(response.data.name);
+                setEmail(response.data.email);
+                setPhone(response.data.phoneNumber);
+                setAge(response.data.age);
+            }
+            catch(err){
+                alert('Error loading student data: ' + err.message);
+                navigate('/students');
+            }
+        }
+        loadStudentData(id);
+    }, [id, authorization, navigate]);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const data = { 
+            name, 
+            email, 
+            phoneNumber: phone, 
+            age 
+        }
+        try {
+            if (id === '0') 
+            {
+                await api.post('api/students', data, authorization);
+            }
+            else 
+            {
+                data.id = studentId;
+                await api.put(`api/students/${studentId}`, data, authorization);
+            }
+            alert('Task completed successfully!');
+        }
+        catch(err){
+            alert('Error submitting student data: ' + err.message);
+        }
+        navigate('/students');
+    }
+
     return (
         <div className="manage-student-container">
             <div className="content">
@@ -16,14 +80,18 @@ export default function ManageStudent() {
                         Back to Students
                     </Link>
                 </section>
-                <form>
-                    <input type='text' placeholder='Name' />
-                    <input type='email' placeholder='Email' />
-                    <input type='tel' placeholder='Phone Number' />
-                    <input type='number' placeholder='Age' />
+                <form onSubmit={handleSubmit}>
+                    <input type='text' placeholder='Name' 
+                        value={name} onChange={e => setName(e.target.value)} />
+                    <input type='email' placeholder='Email' 
+                        value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type='tel' placeholder='Phone Number' 
+                        value={phone} onChange={e => setPhone(e.target.value)} />
+                    <input type='number' placeholder='Age' 
+                        value={age} onChange={e => setAge(e.target.value)} />
                     <button className='button' type='submit'>{id === '0' ? "Add Student" : "Update Student"}</button>
                 </form>
             </div>
         </div>
-    );
+    )
 }
--- react-app/src/components/Students/index.js
+++ react-app/src/components/Students/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import api from '../../services/api';
 import './styles.css';
@@ -8,18 +8,17 @@ import logoRegister from '../../assets/register.png';
 
 export default function Students() {
 
-    const [name, setName] = useState('');
+    //const [name, setName] = useState('');
     const [students, setStudents] = useState([]);
     const navigation = useNavigate();
     
     const email = localStorage.getItem('email');
     const token = localStorage.getItem('token');
-
-    const authorization = {
+    const authorization = useMemo(() => ({
         headers: {
             Authorization: `Bearer ${token}`,
         }
-    };
+    }), [token]);
 
     useEffect(() => {
         async function fetchStudents() {
@@ -46,36 +45,48 @@ export default function Students() {
         }
     }
 
-  return (
-    <div className="student-container">
-        <header>
-            <img src={logoRegister} alt="Register" />
-            <span>Welcome, <strong>{email}</strong>!</span>
-            <Link className="button-link" to="/student/manage/0">Add New Student</Link>
-            <button type="button" onClick={logout} className="button">
-                <FiXCircle size={35} color="#17202a" />
-            </button>
-        </header>
-        <form>
-            <input type="text" placeholder="Search student by name" />
-            <button type="button" className="button">Filter student by name (or part of the name)</button>
-        </form>
-        <h1>List of students</h1>
-        <ul>
-            {students.map(student => (
-                <li key={student.id}>
-                    <b>Name:</b> {student.name}<br /><br />
-                    <b>Email:</b> {student.email}<br /><br />
-                    <b>Phone Number:</b> {student.phoneNumber}<br /><br />
-                    <b>Age:</b> {student.age}<br /><br />
-                    <hr />
-                    <div class="button-container">
-                        <button type="button"><FiEdit size={25} color="#17202a" /></button>
-                        <button type="button"><FiUserX size={25} color="#17202a" /></button>
-                    </div>
-                </li>
-            ))}
-        </ul>
-    </div>
-  );
+    async function editStudent(studentId) {
+        try {
+            navigation(`/student/manage/${studentId}`);
+        } catch (err) {
+            alert('Error editing student: ' + err.message);
+        }
+    }
+
+    return (
+        <div className="student-container">
+            <header>
+                <img src={logoRegister} alt="Register" />
+                <span>Welcome, <strong>{email}</strong>!</span>
+                <Link className="button-link" to="/student/manage/0">Add New Student</Link>
+                <button type="button" onClick={logout} className="button">
+                    <FiXCircle size={35} color="#17202a" />
+                </button>
+            </header>
+            <form>
+                <input type="text" placeholder="Search student by name" />
+                <button type="button" className="button">Filter student by name (or part of the name)</button>
+            </form>
+            <h1>List of students</h1>
+            <ul>
+                {students.map(student => (
+                    <li key={student.id}>
+                        <b>Name:</b> {student.name}<br /><br />
+                        <b>Email:</b> {student.email}<br /><br />
+                        <b>Phone Number:</b> {student.phoneNumber}<br /><br />
+                        <b>Age:</b> {student.age}<br /><br />
+                        <hr />
+                        <div class="button-container">
+                            <button onClick={() => editStudent(student.id)} type="button">
+                                <FiEdit size={25} color="#17202a" />
+                            </button>
+                            <button type="button">
+                                <FiUserX size={25} color="#17202a" />
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
