5bd0d99b72b005b4aab609478ae279b7deb23d0c
Bernardo Albuquerque <bernardo-sw@outlook.com>
2026-01-28 03:17:35 -0300

    Integration has been created between the ReactJS frontend app and the API REST.

--- react-app/src/components/Login/index.js
+++ react-app/src/components/Login/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../../services/api';
 import './styles.css';
 import loginImage from '../../assets/login.png';
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        const data = { email, password };
+        try {
+            const response = await api.post('api/account/loginuser', data);
+
+            localStorage.setItem('email', email);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('expiration', response.data.expiration);
+
+            navigate('/students');
+        } catch (err) {
+            alert('Login failed: ' + err.message);
+        }
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={loginImage} alt="Login" id="login-image" />
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Login</h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
                     <button className="button" type="submit">Enter</button>
                 </form>
             </section>
--- react-app/src/components/Students/index.js
+++ react-app/src/components/Students/index.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import api from '../../services/api';
 import './styles.css';
 
 import { FiEdit, FiUserX, FiXCircle } from 'react-icons/fi';
-
 import logoRegister from '../../assets/register.png';
 
 export default function Students() {
+
+    const [name, setName] = useState('');
+    const [students, setStudents] = useState([]);
+    const navigation = useNavigate();
+    
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    };
+
+    useEffect(() => {
+        async function fetchStudents() {
+            try {
+                const response = await api.get('api/students', authorization);
+                setStudents(response.data); 
+            }
+            catch (err) {
+                alert('Error fetching students: ' + err.message);
+            }
+        }
+        fetchStudents();
+    }, [authorization]);
+
+    async function logout() {
+        try {
+            //await api.post('api/auth/logout', {}, authorization);
+            localStorage.clear();
+            localStorage.setItem('token', '');
+            authorization.headers = { Authorization: '' };
+            navigation('/');
+        } catch (err) {
+            alert('Error logging out: ' + err.message);
+        }
+    }
+
   return (
     <div className="student-container">
         <header>
             <img src={logoRegister} alt="Register" />
-            <span>Welcome, <strong>Bernardo</strong>!</span>
+            <span>Welcome, <strong>{email}</strong>!</span>
             <Link className="button-link" to="/student/manage/0">Add New Student</Link>
-            <button type="button" className="button">
+            <button type="button" onClick={logout} className="button">
                 <FiXCircle size={35} color="#17202a" />
             </button>
         </header>
@@ -23,30 +62,19 @@ export default function Students() {
         </form>
         <h1>List of students</h1>
         <ul>
-            <li>
-                <p>
-                    <b>Name:</b> Bernardo<br /><br />
-                    <b>Email:</b> bernardo-sw@outlook.com<br /><br />
-                    <b>Phone Number:</b> +55 21 96509-5081<br /><br />
-                    <b>Age:</b> 45<br /><br />
+            {students.map(student => (
+                <li key={student.id}>
+                    <b>Name:</b> {student.name}<br /><br />
+                    <b>Email:</b> {student.email}<br /><br />
+                    <b>Phone Number:</b> {student.phoneNumber}<br /><br />
+                    <b>Age:</b> {student.age}<br /><br />
+                    <hr />
                     <div class="button-container">
                         <button type="button"><FiEdit size={25} color="#17202a" /></button>
                         <button type="button"><FiUserX size={25} color="#17202a" /></button>
                     </div>
-                </p>
-            </li>
-            <li>
-                <p>
-                    <b>Name:</b> Bernardo<br /><br />
-                    <b>Email:</b> bernardo-sw@outlook.com<br /><br />
-                    <b>Phone Number:</b> +55 21 96509-5081<br /><br />
-                    <b>Age:</b> 45<br /><br />
-                    <div class="button-container">
-                        <button type="button"><FiEdit size={25} color="#17202a" /></button>
-                        <button type="button"><FiUserX size={25} color="#17202a" /></button>
-                    </div>
-                </p>
-            </li>
+                </li>
+            ))}
         </ul>
     </div>
   );
